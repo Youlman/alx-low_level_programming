6204bac465a155b7151c1d2573f88ffee5c865a7
Youlman <youlmankamal@gmail.com>
2022-09-08 07:03:51 -0700

    C Program that print a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main() {
+
+/**
+ *
+ * main: Entry point
+ * Description: Using function puts to print a string
+ *Return : 0
+ *
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
