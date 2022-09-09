0ea05e5008a9a5bf4ae3fd0079f71aff42b23b75
Youlman <youlmankamal@gmail.com>
2022-09-09 06:44:16 -0700

    Print all single digit ofbase 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit in base
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d",i);
+	}
+	printf("\n");
+	return (0);
+}
