caf185d749f55c3babd09c0b138836e6844d3d89
Youlman <youlmankamal@gmail.com>
2022-09-14 14:01:20 +0000

    Print n times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,17 +18,21 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				product = i * j;
-				if (product < 100)
-					_putchar(' ');
 				if (product < 10)
+				{
 					_putchar(' ');
-				if (product >= 100)
+					_putchar(' ');
+				}
+				else if(product >= 100)
 				{
 					_putchar((product / 100) + '0');
 					_putchar(((product / 10) % 10) + '0');
 				}
 				else if (product < 100 && product >= 10)
+				{
+					_putchar(' ');
 					_putchar((product / 10) + '0');
+				}
 				_putchar((product % 10) + '0');
 			}
 			_putchar('\n');
