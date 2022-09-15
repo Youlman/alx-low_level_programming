b2480c9ce78fa0deb36ab21081780507b98da0f2
Youlman <youlmankamal@gmail.com>
2022-09-15 14:25:09 +0000

    Fibonnaci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - sum  multiple of 3 an 5
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - sum  multiple of 3 an 5
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i, sum = 0;
+	int i0 = 1, i1 = 2 j = 2;
+
+	while (j <= 50)
+	{
+		
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
