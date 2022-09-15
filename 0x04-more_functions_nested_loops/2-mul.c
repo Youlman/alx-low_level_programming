1d17d6f500ba989984e8c98bbe43a224d451d215
Youlman <youlmankamal@gmail.com>
2022-09-15 20:43:11 +0000

    Multiplication of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - Check for a digit
+* @a: first integer
+* @b: second integer
+* Return: number
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
