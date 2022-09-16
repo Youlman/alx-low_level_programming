e2e1eef7568d278e967215163a63f50420f18556
Youlman <youlmankamal@gmail.com>
2022-09-16 09:55:04 +0000

    Fix for all dates and for all leap

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,15 +22,6 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else if ((year % 4 == 0 &&  year % 400 == 0) && (year % 100 == 0))
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
 	else
 	{
 		if ((month == 2 && day == 60) && (year % 100 == 0))
@@ -40,6 +31,16 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else if ((month == 2 && day == 60) && !(year % 100 == 0))
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		else if ((year % 4 == 0 && year % 400 == 0) && (year % 100 == 0))
+		{
+			if (month >= 2 && day >= 60)
+			{
+				day++;
+			}
+
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
