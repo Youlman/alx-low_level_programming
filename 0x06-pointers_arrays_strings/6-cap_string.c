ced3d4151ae90a18695a525f88444421f239a0c9
Youlman <youlmankamal@gmail.com>
2022-09-22 04:15:42 -0700

    Capitalize each word of string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of a string.
+ * @s: the string to change
+ *
+ * Return: a string with words capitalize
+ */
+
+char *cap_string(char *s)
+{
+	int index = 0;
+
+	while (s[index])
+	{
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+		if (
+			s[index - 1] == ' ' ||
+			s[index - 1] == '\t' ||
+			s[index - 1] == '\n' ||
+			s[index - 1] == ',' ||
+			s[index - 1] == ';' ||
+			s[index - 1] == '.' ||
+			s[index - 1] == '!' ||
+			s[index - 1] == '?' ||
+			s[index - 1] == '"' ||
+			s[index - 1] == '(' ||
+			s[index - 1] == ')' ||
+			s[index - 1] == '{' ||
+			s[index - 1] == '}')
+		{
+			s[index] -= 32;
+		}
+		index++;
+	}
+
+	return (s);
+}
