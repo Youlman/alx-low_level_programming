1127ddbb05a5031ce6f651efa80c0a2ffccbf07d
Youlman <youlmankamal@gmail.com>
2022-09-30 09:24:45 -0700

    Create the static library

---
+++ 0x09-static_libraries/test/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - if a character is lowercase
+* @c: The character to check
+*
+* Return: 1 or 0
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
