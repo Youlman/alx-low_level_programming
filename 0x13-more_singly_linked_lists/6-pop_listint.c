fd0ffe0d9cf32fba31913fc810a8a83b67701433
Youlman <youlmankamal@gmail.com>
2022-10-24 23:28:14 +0000

    Delete the head node of list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,12 +13,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *node_delete;
+	int head_node_data;
 
 	if (*head == NULL)
 		return (0);
 
 	node_delete = *head;
 	*head = (*head)->next;
+	head_node_date = node_delete->n;
+
+	free(node_delete);
 
 	return (node_delete->n);
 }
