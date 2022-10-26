07a37631bb13542750bdef878114e995c6d0041c
Youlman <youlmankamal@gmail.com>
2022-10-26 15:37:33 +0000

    Add a new node at a index in linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - Add a new node to the index idx of linked list.
+ * @head: the list .
+ * @idx: the index
+ * @n: an integer
+ *
+ * Return: NULL or the address of the new element
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *tmp = *head;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = tmp;
+		*head = new_node;
+		return (new_node);
+	}
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (tmp == NULL || tmp->next == NULL)
+			return (NULL);
+
+		tmp = tmp->next;
+	}
+
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+
+	return (new_node);
+}
